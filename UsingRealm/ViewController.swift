629c8ef0e2e07a4ae14efa9aa33bef1e5d3844ed
magnuskahr <magnuskahr@icloud.com>
2018-01-27 00:20:28 +0100

    Initial Commit

---
+++ UsingRealm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingRealm
+//
+//  Created by Magnus Jensen on 27/01/2018.
+//  Copyright © 2018 Magnus Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UsingRealmTests/UsingRealmTests.swift
@@ -0,0 +1,36 @@
+//
+//  UsingRealmTests.swift
+//  UsingRealmTests
+//
+//  Created by Magnus Jensen on 27/01/2018.
+//  Copyright © 2018 Magnus Jensen. All rights reserved.
+//
+
+import XCTest
+@testable import UsingRealm
+
+class UsingRealmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
